7a1891ebb7720b6df9f1dfdc1526a490d3f923d8
Serge <sergengoga05@gmail.com>
2024-10-19 00:05:02 +0200

    implemented RBAC

--- src/main/java/com/serge/votingapp/service/UserService.java
+++ src/main/java/com/serge/votingapp/service/UserService.java
@@ -4,6 +4,15 @@ import com.serge.votingapp.model.User;
 import com.serge.votingapp.web.dto.UserRegistrationDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto registrationDto);
+    public List<User> getUsers();
+
+    void deleteUserById(Long id);
+
+    User getUserById(Long id);
+
+    public void updateUser(Long id, User updatedUser);
 }
