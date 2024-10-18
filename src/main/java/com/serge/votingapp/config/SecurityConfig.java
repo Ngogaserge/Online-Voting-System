3eaf35de928a1da86c32d62ed48b5bd397228692
Serge <sergengoga05@gmail.com>
2024-10-18 23:59:39 +0200

    implemented RBAC

--- src/main/java/com/serge/votingapp/config/SecurityConfig.java
+++ src/main/java/com/serge/votingapp/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -33,11 +34,16 @@ public class SecurityConfig {
                                 "/css/",
                                 "/img/"
                         ).permitAll()
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")  // Only ADMIN role can access /admin routes
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")  // Both USER and ADMIN can access /user routes
+                        .requestMatchers("/voter/**").hasAnyRole("USER")
                         .anyRequest().authenticated()
 
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .successHandler(customAuthenticationSuccessHandler())
                         .permitAll()
                 )
                 .logout(logout -> logout
@@ -55,5 +61,9 @@ public class SecurityConfig {
         auth.setUserDetailsService(userService);
         auth.setPasswordEncoder(passwordEncoder);  // Now injected from PasswordEncoderConfig
         return auth;
+    }
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
     }
 }
